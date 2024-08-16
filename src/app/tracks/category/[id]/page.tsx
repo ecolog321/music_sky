6f9921d03c23cd5b07f71a3c759767cb2a7de808
ecolog321 <ecakrhipov@gmail.com>
2024-08-16 22:00:37 +0800

    add categories

--- src/app/tracks/category/[id]/page.tsx
+++ src/app/tracks/category/[id]/page.tsx
@@ -1,13 +1,42 @@
-"use client"
+"use client";
 
 import { CenterBlock } from "@components/Main/CenterBlock/CenterBlock";
+import { useParams, useRouter } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/store";
+import { useEffect, useState } from "react";
+import { getCategoryPlaylist } from "@api/categoryApi";
+import { setPlaylist } from "../../../../store/features/playlistSlise";
+import { TrackType } from "../../../../types/types";
 
+export default function CategoryPage() {
+  const id = useParams()?.id;
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const access = useAppSelector((store) => store.auth.tokens.access);
+  const tracks = useAppSelector((store) => store.playlist.filteredPlaylist);
+  const [newTracks, setNewTracks] = useState<TrackType[]>([]);
 
+  if (!access) {
+    router.push("/singin");
+  }
 
-export default function FavoritePage() {
+  console.log(tracks);
 
+  useEffect(() => {
+    if (access) {
+      getCategoryPlaylist(access, id).then((response) => {
+        const tracksId: [] = response.data.items;
+        for (let index = 0; index < tracks.length; index++) {
+          const addTrack: TrackType | undefined = tracks.find(
+            (track) => track._id === tracksId[index]
+          );
+          addTrack ? setNewTracks([...newTracks, addTrack]) : null;
+        }
 
-  
+        dispatch(setPlaylist({tracks:newTracks}))
 
-  return <CenterBlock tracks={[]}/>;
+      });
+    }
+  }, [access, id, dispatch, tracks, newTracks]);
+  return <CenterBlock tracks={tracks} />;
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,23 @@
+"use client";
 import Image from "next/image";
 import styles from "./sidebar.module.css";
 import { User } from "./User/User";
+import { useRouter } from "next/navigation";
 
 export const Sidebar = () => {
+  const router = useRouter();
+
+  const goToPlaylist = (id: number) => {
+    router.push(`/tracks/category/${id}`);
+  };
+
   return (
     <div className={styles.main__sidebar}>
-      <User/>
+      <User />
       <div className={styles.sidebar__block}>
         <div className={styles.sidebar__list}>
-          <div className={styles.sidebar__item}>
-            <a className={styles.sidebar__link} href="">
+          <div className={styles.sidebar__item} onClick={() => goToPlaylist(1)}>
+            <a className={styles.sidebar__link}>
               <Image
                 className={styles.sidebar__img}
                 src="/img/playlist01.png"
@@ -19,8 +27,8 @@ export const Sidebar = () => {
               />
             </a>
           </div>
-          <div className={styles.sidebar__item}>
-            <a className={styles.sidebar__link} href="">
+          <div className={styles.sidebar__item} onClick={() => goToPlaylist(2)}>
+            <a className={styles.sidebar__link}>
               <Image
                 className={styles.sidebar__img}
                 src="/img/playlist02.png"
@@ -30,8 +38,8 @@ export const Sidebar = () => {
               />
             </a>
           </div>
-          <div className={styles.sidebar__item}>
-            <a className={styles.sidebar__link} href="">
+          <div className={styles.sidebar__item} onClick={() => goToPlaylist(3)}>
+            <a className={styles.sidebar__link}>
               <Image
                 className={styles.sidebar__img}
                 src="/img/playlist03.png"
